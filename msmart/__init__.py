e4be1dfe5d3b62bcddf83b8d811ebabb93ae008b
Tucker Kern <tuckkern@gmail.com>
2023-09-07 10:36:40 -0600

    Add a check for the legacy msmart package on import

--- msmart/__init__.py
+++ msmart/__init__.py
@@ -1,3 +1,11 @@
 from importlib import metadata
 
 __version__ = metadata.version("msmart-ng")
+
+# Guard against legacy msmart package which may cause conflicts
+try:
+    metadata.version("msmart")
+    raise ImportError("Legacy msmart packaged detected. Please remove.")
+except metadata.PackageNotFoundError:
+    # Good, legacy package is missing
+    pass
