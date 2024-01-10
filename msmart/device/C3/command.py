cf575320d7c2192abde6174f3d9e08455e9f3fe6
Tucker Kern <tuckkern@gmail.com>
2024-01-10 09:25:43 -0700

    Add basic control command

--- msmart/device/C3/command.py
+++ msmart/device/C3/command.py
@@ -66,3 +66,69 @@ class QueryEcoCommand(QueryCommand):
 
     def __init__(self) -> None:
         super().__init__(QueryType.QUERY_ECO)
+
+
+class ControlCommand(Command, ABC):
+    """Base class for control commands."""
+
+    def __init__(self, type: ControlType) -> None:
+        super().__init__(DeviceType.HEAT_PUMP, frame_type=FrameType.REQUEST)
+
+        self._type = type
+
+
+class ControlBasicCommand(ControlCommand):
+    """Command to control basic device state."""
+
+    def __init__(self) -> None:
+        super().__init__(ControlType.CONTROL_BASIC)
+
+        self.zone1_power_state = False
+        self.zone2_power_state = False
+        self.dhw_power_state = False
+
+        self.run_mode_set = 0  # TODO??
+
+        # TODO default values?
+        self.zone1_target_temperature = 0
+        self.zone2_target_temperature = 0
+        self.dhw_target_temperature = 0
+        self.room_target_temperature = 0
+
+        self.zone1_curve_state = False
+        self.zone2_curve_state = False
+
+        # TODO forcetbh_state
+        self.fastdhw_state = False
+
+        # TODO "newfunction_en"
+        self.zone1_curve_type = 0  # TODO??
+        self.zone2_curve_type = 0
+
+    @property
+    def payload(self) -> bytes:
+
+        payload = [0] * 10
+
+        payload[0] = self._type
+
+        payload[1] |= 1 << 0 if self.zone1_power_state else 0
+        payload[1] |= 1 << 1 if self.zone2_power_state else 0
+        payload[1] |= 1 << 2 if self.dhw_power_state else 0
+
+        payload[2] = self.run_mode_set
+        payload[3] = self.zone1_target_temperature
+        payload[4] = self.zone2_target_temperature
+        payload[5] = self.dhw_target_temperature
+        payload[6] = self.room_target_temperature * 2  # TODO ??
+
+        payload[7] |= 1 << 0 if self.zone1_curve_state else 0
+        payload[7] |= 1 << 1 if self.zone2_curve_state else 0
+        # payload[7] |= 1 << 2 if self.forcetbh_state else 0
+        payload[7] |= 1 << 3 if self.fastdhw_state else 0
+
+        # TODO newfunction_en
+        payload[8] = self.zone1_curve_type
+        payload[9] = self.zone2_curve_type
+
+        return bytes(payload)
