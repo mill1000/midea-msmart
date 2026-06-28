b423f5fdf8f956a132956a2c7c0d836039820436
Tucker Kern <tuckkern@gmail.com>
2026-06-28 04:00:33 +0000

    Remove old reader

--- msmart/device/AC/command.py
+++ msmart/device/AC/command.py
@@ -572,7 +572,6 @@ class CapabilitiesResponse(Response):
                        v in [1, 2, 4, 6, 7, 9, 10, 11, 12, 13]),
                 reader("cool_mode", lambda v:
                        v in [0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15]),
-                # reader("cool_mode", lambda v: v[0] not in [2, 10, 12]),
                 reader("dry_mode", lambda v:
                        v in [0, 1, 5, 6, 9, 11, 13, 14, 15]),
                 reader("auto_mode", lambda v:
